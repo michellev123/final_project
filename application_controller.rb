59dc091db079b3a92412037dbb73ba666ea5f10b
Michelle <you@example.com>
2015-07-16 15:19:36 +0000

    update

--- application_controller.rb
+++ application_controller.rb
@@ -6,30 +6,28 @@ require './models/model.rb'
 class MyApp < Sinatra::Base
 
   get '/' do
-    erb :index
+    erb :final_index
   end
   
-  get '/results' do
-    erb :results 
-  end 
-  
-  get '/link' do
-    erb :link
-  end 
-  
-  get '/apples' do 
-    erb :aples 
-  end 
-  
-  get '/food' do
-    @foods = ["macaroni", "pizza", "brownies"]
-    erb :food
-  end 
-  
-  get '/doge' do
-    @doge1 = Doge.new("Sparkles")
-    erb :doge
     
-    
+    post '/results' do
+      puts params
+      @traveldestination = params[:traveldestination]
+      @dates = params[:dates]
+      @quantity = params[:dates] * 1
+      @activities = params[:activities]
+     
+      @sentences = ["Essentials include: #{@quantity} pairs of underwear, #{@quantity} casual shirts, #{@quantity} casual bottoms, belt, sunglasses, wallet, passport, keys, ID, watch, books, camera, phone, umbrella, phone, phone charger, disinfectant, contacts, perfume, cologne, blemish stick, makeup, deoderant, toothpaste, toothbrush, floss, razor, shaving cream, q-tips, moisturizer, cleanser."]
+      
+      @sentence_hash = {"Swimming"=>"For swimming, bring: bathing suit, sunglasses, flipflops.", "Work"=>"For work, bring: work phone, laptop, laptop charger, business cards.", "Beach"=>"For the beach, bring: sunblock, sunglasses, bathing suit, towel, hat, sandals.", "Hiking"=>"For hiking, bring: sneakers or hiking boots, socks (not cotton), water bottle, hat, insect repellant, first aid kit, t-shirt.", "Elegant Dinner or Event"=>"For an elegant dinner or event, bring: dress(long)/skirt or suit, high heels or dress shoes, jewelery, cufflinks.", "Fitness"=>"For fitness, bring: water bottle, leggings or shorts, t-shirt or other gear, sports bra, sweatshirt, sneakers.", "Newborn"=> "If you're bringing a newborn, bring: blankets, diapers, pacifier, toys, milk, breast pump, bottles, lotion, diaper rash cream, baby shampoo, wipes, onesie, socks, sweater, bib, baby monitor.", "Winter Sports"=>"For winter sports, bring: sweater, ski pants, helmet, wool socks, gloves, scarf, sunscreen, ski jacket, moisturizer."}
+      
+      @activities.each do |activity|
+        @sentences.push(@sentence_hash[activity])
+      end 
+      
+      puts @sentences
+
+      
+      
   end 
 end
