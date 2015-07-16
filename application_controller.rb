8f6dd1b4f2f25e03f5556bc8bb1526b101b23527
cbeck18 <cbeck18@spenceschool.org>
2015-07-16 18:10:24 +0000

    aogh

--- application_controller.rb
+++ application_controller.rb
@@ -19,7 +19,6 @@ class MyApp < Sinatra::Base
         "Hiking"=>"For hiking, bring: sneakers or hiking boots, socks (not cotton), water bottle, hat, insect repellant, first aid kit, t-shirt.", 
         "Elegant Dinner or Event"=>"For an elegant dinner or event, bring: dress(long)/skirt or suit, high heels or dress shoes, jewelery, cufflinks.", 
         "Fitness"=>"For fitness, bring: water bottle, leggings or shorts, t-shirt or other gear, sports bra, sweatshirt, sneakers.", 
-        "Newborn"=> "If you're bringing a newborn, bring: blankets, diapers, pacifier, toys, milk, breast pump, bottles, lotion, diaper rash cream, baby shampoo, wipes, onesie, socks, sweater, bib, baby monitor.", 
         "Winter Sports"=>"For winter sports, bring: sweater, ski pants, helmet, wool socks, gloves, scarf, sunscreen, ski jacket, moisturizer." }
       
         packer1 = Packer.new(params[:traveldestination], params[:dates], params[:dates] * 1, params[:activities], sentence_hash)
