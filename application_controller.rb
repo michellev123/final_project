b93ce3ae8eb40bdd12cb9d9ab980773f4c2e91b0
cbeck18 <cbeck18@spenceschool.org>
2015-07-17 14:55:35 +0000

    DONEEEEEEEEEE YAAAAAAAASSSSSS

--- application_controller.rb
+++ application_controller.rb
@@ -9,17 +9,17 @@ class MyApp < Sinatra::Base
     erb :index
   end
   
-    
+
     post '/results' do
    
       
-      sentence_hash =  { "Swimming"=>"For swimming, bring: bathing suit, sunglasses, flipflops.", 
-        "Work"=>"For work, bring: work phone, laptop, laptop charger, business cards.", 
-        "Beach"=>"For the beach, bring: sunblock, sunglasses, bathing suit, towel, hat, sandals.",
-        "Hiking"=>"For hiking, bring: sneakers or hiking boots, socks (not cotton), water bottle, hat, insect repellant, first aid kit, t-shirt.", 
-        "Elegant Dinner or Event"=>"For an elegant dinner or event, bring: dress(long)/skirt or suit, high heels or dress shoes, jewelery, cufflinks.", 
-        "Fitness"=>"For fitness, bring: water bottle, leggings or shorts, t-shirt or other gear, sports bra, sweatshirt, sneakers.", 
-        "Winter Sports"=>"For winter sports, bring: sweater, ski pants, helmet, wool socks, gloves, scarf, sunscreen, ski jacket, moisturizer." }
+      sentence_hash =  { "Swimming"=>"FOR SWIMMING:    bathing suit, sunglasses, flipflops.", 
+        "Work"=>"FOR WORK:    work phone, laptop, laptop charger, business cards.", 
+        "Beach"=>"FOR THE BEACH:    sunblock, sunglasses, bathing suit, towel, hat, sandals.",
+        "Hiking"=>"FOR HIKING:    sneakers or hiking boots, socks (not cotton), water bottle, hat, insect repellant, first aid kit, t-shirt.", 
+        "Elegant Dinner or Event"=>"FOR AN ELEGANT DINNER OR EVENT:    dress(long)/skirt or suit, high heels or dress shoes, jewelery, cufflinks.", 
+        "Fitness"=>"FOR FITNESS:    water bottle, leggings or shorts, t-shirt or other gear, sports bra, sweatshirt, sneakers.", 
+        "Winter Sports"=>"FOR WINTER SPORTS:    sweater, ski pants, helmet, wool socks, gloves, scarf, sunscreen, ski jacket, moisturizer." }
       
         packer1 = Packer.new(params[:traveldestination], params[:dates], params[:dates] * 1, params[:activities], sentence_hash)
       
--- models/model.rb
+++ models/model.rb
@@ -7,7 +7,7 @@ class Packer
     @dates = dates
     @quantity = quantity
     @activities = activities
-    @sentences = "Essentials include: #{@quantity} pairs of underwear, #{@quantity} casual shirts, #{@quantity} casual bottoms, belt, sunglasses, wallet, passport, keys, ID, watch, books, camera, phone, umbrella, phone, phone charger, disinfectant, contacts, perfume, cologne, blemish stick, makeup, deoderant, toothpaste, toothbrush, floss, razor, shaving cream, q-tips, moisturizer, cleanser."
+    @sentences = "ESSENTIALS INCLUDE: #{@quantity} pairs of underwear, #{@quantity} casual shirts, #{@quantity} casual bottoms, belt, sunglasses, wallet, passport, keys, ID, watch, books, camera, phone, umbrella, phone, phone charger, disinfectant, contacts, perfume, cologne, blemish stick, makeup, deoderant, toothpaste, toothbrush, floss, razor, shaving cream, q-tips, moisturizer, cleanser."
     @sentence_hash = sentence_hash
   end
   
